118e67a8b4a67121953209729323cf87947b0e48
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2021-08-18 20:21:06 -0300

    Projeto Criado - Teste

---
+++ src/main/java/com/meustestes/alomundo/AlomundoApplication.java
@@ -0,0 +1,13 @@
+package com.meustestes.alomundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlomundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlomundoApplication.class, args);
+	}
+
+}
